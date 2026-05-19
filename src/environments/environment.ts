801f4d482e68da0e6472adf80e35c942604afa01
piccolofrancesco39@gmail.com <piccolofrancesco39@gmail.com>
2026-05-19 22:51:14 +0200

    enviroment

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const env = {
+  production: false,
+
+  firebaseConfig: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
+  }
+};
