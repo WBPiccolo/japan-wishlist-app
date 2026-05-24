f217cfe03917450da6585af3e616fee46e5990f0
WBPiccolo <piccolofrancesco39@gmail.com>
2026-05-24 22:05:43 +0200

    fix env prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const env = {
+   production: false,
+   firebaseConfig: {
+      apiKey: 'AIzaSyBsSK3sxNcllLZgGpO7rKDUOhLIKYM4VTs',
+      authDomain: 'japan-wishlist.firebaseapp.com',
+      projectId: 'japan-wishlist',
+      storageBucket: 'japan-wishlist.firebasestorage.app',
+      messagingSenderId: '183314425089',
+      appId: '1:183314425089:web:b80453e3bb4582cbb2e700',
+   },
+}
