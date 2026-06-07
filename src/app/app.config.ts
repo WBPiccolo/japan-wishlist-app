26d43cdfeaf5e680a437cea08ee13f8486f72af9
piccolofrancesco39@gmail.com <piccolofrancesco39@gmail.com>
2026-06-07 13:43:59 +0200

    bugfix currency

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core'
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core'
 import { provideRouter } from '@angular/router'
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app'
 import { routes } from './app.routes'
@@ -7,9 +7,14 @@ import { env } from '../environments/environment'
 import { providePrimeNG } from 'primeng/config'
 import Aura from '@primeuix/themes/aura'
 import { ConfirmationService, MessageService } from 'primeng/api'
+import { registerLocaleData } from '@angular/common'
+import localeIt from '@angular/common/locales/it'
+
+registerLocaleData(localeIt)
 
 export const appConfig: ApplicationConfig = {
    providers: [
+      { provide: LOCALE_ID, useValue: 'it-IT' },
       provideBrowserGlobalErrorListeners(),
       provideRouter(routes),
       provideFirebaseApp(() => initializeApp(env.firebaseConfig)),
